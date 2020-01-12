932c8af52122e5c7865cbd16d31cfecb999089c0
Norwin <norwin.dijkman@plathena.com>
2020-01-12 14:57:38 +0100

    custom contact page

--- src/cms/preview-templates/ContactPagePreview.js
+++ src/cms/preview-templates/ContactPagePreview.js
@@ -2,24 +2,15 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { ContactPageTemplate } from '../../templates/contact-page'
 
-const ContactPagePreview = ({ entry, widgetFor }) => {
-  const data = entry.getIn(['data']).toJS()
-
-  if (data) {
-    return (
-      <ContactPageTemplate
-        content={widgetFor('body')}
-      />
-    )
-  } else {
-    return <div>Loading...</div>
-  }
+const ContactPagePreview = ({ widgetFor }) => {
+  return (
+    <ContactPageTemplate
+      content={widgetFor('body')}
+    />
+  )
 }
 
 ContactPagePreview.propTypes = {
-  entry: PropTypes.shape({
-    getIn: PropTypes.func,
-  }),
   widgetFor: PropTypes.func,
 }
 
