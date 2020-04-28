ee829a2c0dc6f8753e5aeed103e35ba1c637913a
Norwin <norwin.dijkman@plathena.com>
2020-04-28 21:59:16 +0200

    ga

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,6 +10,13 @@ module.exports = {
     'gatsby-plugin-react-helmet',
     'gatsby-plugin-sass',
     {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-97788127-2",
+        cookieDomain: "juniorjoy.nl",
+      },
+    },
+    {
       // keep as first gatsby-source-filesystem plugin for gatsby image support
       resolve: 'gatsby-source-filesystem',
       options: {
