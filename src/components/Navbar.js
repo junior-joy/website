03269455f68fbef2a2f7d84ee7cd89c79fe4af1f
junior-joy <59141656+junior-joy@users.noreply.github.com>
2020-04-29 13:40:44 +0200

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -152,8 +152,8 @@ const Navbar = class extends React.Component {
                 </ExpansionPanelDetails>
               </ExpansionPanel>
               <div className="navbar-start has-text-centered">
-                <Link className="navbar-item" to="/blog" activeClassName="is-active" partiallyActive={true}>
-                  Blog
+                <Link className="navbar-item" to="/baanplanner/" activeClassName="is-active" partiallyActive={true}>
+                  Baanplanner
                 </Link>
                 <Link className="navbar-item" to="/contact" activeClassName="is-active" partiallyActive={true}>
                   Contact
