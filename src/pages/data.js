a62578b5782613c1b1fee97e13fba99133b4c002
junior-joy <59141656+junior-joy@users.noreply.github.com>
2020-09-18 08:37:35 +0200

    Update data.js

--- src/pages/data.js
+++ src/pages/data.js
@@ -6,4 +6,6 @@ title: Data zomer 2020
 import Button from '@material-ui/core/button';
 import { link } from 'gatsby'
 
+<link to="/inhaal><Button variant="contained" primary className="btn">inhaaldata</Button></link>
+
 ![](https://res.cloudinary.com/junior-joy/image/upload/v1593025464/Schermafbeelding_2020-06-24_om_21.03.55_qpocvh.png)
