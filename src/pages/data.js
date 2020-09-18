576da1a6db3a4481808918316f52c481970525f6
junior-joy <59141656+junior-joy@users.noreply.github.com>
2020-09-18 08:33:33 +0200

    Update and rename data.md to data.js

--- src/pages/data.js
+++ src/pages/data.js
@@ -3,4 +3,7 @@ templateKey: about-page
 title: Data zomer 2020
 ---
 
+import Button from '@material-ui/core/button';
+import { link } from 'gatsby'
+
 ![](https://res.cloudinary.com/junior-joy/image/upload/v1593025464/Schermafbeelding_2020-06-24_om_21.03.55_qpocvh.png)
